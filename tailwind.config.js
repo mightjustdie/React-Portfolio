6e127c207cf2a7e11b9b216b7eaf6a296875ae89
Brenden <brenthomas02@gmail.com>
2023-06-12 20:17:28 -0400

    still setting up contact page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    container: {
-      center: true,
-    },
-    extend: {
-      backgroundImage: {
-      'port-bg': "url('../assets/img/port-bg.jpg')",
-      }
+    // container: {
+    //   center: true,
+    // },
+    // extend: {
+    //   backgroundImage: {
+    //   'port-bg': "url('../assets/img/port-bg.jpg')",
+    //   }
     },
   },
   plugins: [],
